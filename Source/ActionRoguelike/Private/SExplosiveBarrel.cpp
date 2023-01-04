8e8ac113923574a4cb3d99ab4da7c103d78f414f
Shifeng <s.liang.dev@gmail.com>
2023-01-04 00:26:35 -0800

    Added explosive barrel

--- Source/ActionRoguelike/Private/SExplosiveBarrel.cpp
+++ Source/ActionRoguelike/Private/SExplosiveBarrel.cpp
@@ -2,6 +2,7 @@
 
 
 #include "SExplosiveBarrel.h"
+#include "PhysicsEngine/RadialForceComponent.h"
 
 // Sets default values
 ASExplosiveBarrel::ASExplosiveBarrel()
@@ -9,6 +10,14 @@ ASExplosiveBarrel::ASExplosiveBarrel()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	StaticMeshComp = CreateDefaultSubobject<UStaticMeshComponent>("StaticMeshComp");
+	RootComponent = StaticMeshComp;
+
+	RadialForceComp = CreateDefaultSubobject<URadialForceComponent>("Radial Force");
+	RadialForceComp->Radius = 400.f;
+	RadialForceComp->ImpulseStrength = 2000.f;
+	RadialForceComp->bImpulseVelChange = true;
+	RadialForceComp->SetupAttachment(RootComponent);
 }
 
 // Called when the game starts or when spawned
